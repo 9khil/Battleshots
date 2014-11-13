d7b2584f3daf737d3a6788864ada46c4ff79d773
Nikhil André Luthra <nikhil.a.luthra@gmail.com>
2014-11-13 22:17:13 +0100

    updating server to handle requests from multiple clients

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
 var serverPort = 1234,
-	players = [];
+	players = {};
 
 server.listen(serverPort);
 
@@ -18,13 +18,26 @@ io.on('connection', function (socket) {
 
   socket.emit('event', { hello: 'world' });
   socket.on('identification', function (data) {
-    console.log(data);
+   	if(addPlayer(data, socket.conn.id)){
+   		//succes
+   		console.log("success");
+   		io.sockets.connected[socket.conn.id].emit('event', {hello: 'world'}); //only sending to requester
+   		
+   	}
+   	else{
+   		//error. only emit to client
+   	}
+    
   });
 });
 
-function addPlayer(){
-	if(players.length === 0){
-
+function addPlayer(playerName, connectionID){
+	
+	if(io.eio.clients[connectionID]){
+		return true;
+	}
+	else{
+		return false;
 	}	
 }
 
