a972c8be551495654e3b030acdfd66e728cd7ab4
Empyreum <stian.forberg@WIN81-AG28FHPH.corp.avanade.org>
2015-10-21 18:41:38 +0200

    Mapping of positinos to lednumbers and color to RGB value. Used by calling setLight(positionalArray, color).

--- client/lights.js
+++ client/lights.js
@@ -1,41 +1,53 @@
-var map = require('./gridMapper.js');
-
+//var map = require('./gridMapper.js');
 
 function controlLights(grid, color){
-
-  var pos;
-
-  for(var i=0; i<grid.length;i++){
-    pos.push(map.gridMapper(grid[i]));
-  }
-
-  setLights(pos, color);
-
-
+  //console.log("controlLights");
+  var pos = new Array();
+  pos = gridMapper(grid);
+  setLights(gridMapper(grid), mapColor(color));
+  return true;
 }
-
 
 function setLights(pos, color){
+  //console.log("setLights");
     for(var j=0; j<pos.length; j++){
-      console.log(pos[j] + " " + color)
-      //leds.setColor(pos[j], color)
+      console.log("Setting ledNr " + pos[j] + " to " + color)
+      leds.setColor(pos[j], color);
     }
+    leds.update();
 }
 
 
-function setLight(pos, color){
- //console.log("in setLight");
- for(var j=0; j<pos.length; j++){
-   var tempLed = pos[j];
-   //console.log("Setting led " + pos + " to red");
-   for(i = 0; i<numberLeds; i++){
-     if(i == tempLed){
-       leds.setColor(i, [255,0,0]);
-     }else{
-       // leds.setColor(i, [0,0,255]);
-     }
-    }
-    leds.update();
+// function setLight(pos, color){
+//  //console.log("in setLight");
+//  for(var j=0; j<pos.length; j++){
+//    var tempLed = pos[j];
+//    //console.log("Setting led " + pos + " to red");
+//    for(i = 0; i<numberLeds; i++){
+//      if(i == tempLed){
+//        leds.setColor(i, [255,0,0]);
+//      }else{
+//        // leds.setColor(i, [0,0,255]);
+//      }
+//     }
+//     leds.update();
+//   }
+// }
+
+function mapColor(color){
+  switch(color){
+    case "White":
+      return ColorEnum.White;
+    case "Blue":
+      return ColorEnum.Blue;
+    case "Green":
+      return ColorEnum.Green;
+    case "Red":
+      return ColorEnum.Red;
+    case "Black":
+      return ColorEnum.Black;
+    default :
+      return ColorEnum.Black;
   }
 }
 
@@ -46,4 +58,55 @@ ColorEnum = {
   Green : "[0,255,0]",
   Red : "[255,0,0]",
   Black : "[0,0,0]"
+}
+
+function gridMapper(inputArray){
+  //console.log("gridMapper");
+  var ledNumber = 0;
+  var ledArray = [];
+
+  for(var i=0; i<inputArray.length;i++){
+  var input = inputArray[i];
+    var _abc = input.substring(0,1);
+    var _123 = parseInt(input.substring(1));
+
+    switch(_abc){
+      case "A":
+        ledNumber = 0 + _123-1;
+        break;
+      case "B":
+        ledNumber = 10 + (10 - _123);
+        break;
+      case "C":
+        ledNumber = 20 + _123-1;
+        break;
+      case "D":
+        ledNumber = 30 + (10 - _123);
+        break;
+      case "E":
+        ledNumber = 40 + _123-1;
+        break;
+      case "F":
+        ledNumber = 50 + (10 - _123);
+        break;
+      case "G":
+        ledNumber = 60 + _123-1;
+        break;
+      case "H":
+        ledNumber = 70 + (10 - _123);
+        break;
+      case "I":
+        ledNumber = 80 + _123-1;
+        break;
+      case "J":
+        ledNumber = 90 + (10 - _123);
+        break;
+      default:
+        ledNumber = 0;
+        break;
+    }
+  ledArray.push((ledNumber));
+  }
+  return ledArray;
+  //console.log(ledNumber)
 }
