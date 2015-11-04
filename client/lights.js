91abd4427e9ba0bc3b2b95c43d0e82f0fc4df559
Empyreum <stian.forberg@WIN81-AG28FHPH.corp.avanade.org>
2015-11-04 19:30:51 +0100

    Now using object incomming from server

--- client/lights.js
+++ client/lights.js
@@ -1,26 +1,50 @@
 //var map = require('./gridMapper.js');
 var leds = require('rpi-ws2801');
-var ColorEnum = {
-  White : "[255,255,255]",
-  Blue : "[0,0,255]",
-  Green : "[0,255,0]",
-  Red : "[255,0,0]",
-  Black : "[0,0,0]"
+
+LETTERS = {
+    1: "A",
+    2: "B",
+    3: "C",
+    4: "D",
+    5: "E",
+    6: "F",
+    7: "G",
+    8: "H",
+    9: "I",
+    10: "J"
 };
 
-//test
-controlLights(["A1", "A2", "A3", "A4", "A5", "A6", "A7", "A8", "A9", "A10"], "White");
-controlLights(["B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9", "B10"], "Blue");
-controlLights(["C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10"], "Green");
-controlLights(["D1", "D2", "D3", "D4", "D5", "D6", "D7", "D8", "D9", "D10"], "Red");
-controlLights(["E1", "E2", "E3", "E4", "E5", "E6", "E7", "E8", "E9", "E10"], "Black");
-controlLights(["F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10"], "White");
-controlLights(["G1", "G2", "G3", "G4", "G5", "G6", "G7", "G8", "G9", "G10"], "Blue");
-controlLights(["H1", "H2", "H3", "H4", "H5", "H6", "H7", "H8", "H9", "H10"], "Green");
-controlLights(["I1", "I2", "I3", "I4", "I5", "I6", "I7", "I8", "I9", "I10"], "Red");
-controlLights(["J1", "J2", "J3", "J4", "J5", "J6", "J7", "J8", "J9", "J10"], "Black");
 
+init();
 
+function init(){
+  grid = {};
+  for (var i in LETTERS) {
+    grid[LETTERS[i]] = {};
+    for (var num = 1; num <= 10; num++) {
+       grid[LETTERS[i]][num] = 1;
+    }
+  }
+  controlLights(grid);
+}
+
+function controlLights(grid){
+
+  for(var i=0; i<=99;i++){
+
+    var coord = newGridMapper(i);
+
+    setLight(i, colorFunction(grid[coord[0]][coord[1]]));
+
+  }
+  leds.update();
+}
+
+function setLight(pos, color){
+  leds.setColor(pos, color);
+}
+
+/*
 function controlLights(grid, color){
   //console.log("controlLights");
   var pos = new Array();
@@ -33,11 +57,13 @@ function setLights(pos, color){
   //console.log("setLights");
     for(var j=0; j<pos.length; j++){
       console.log("Setting ledNr " + pos[j] + " to " + color)
+
+
       leds.setColor(pos[j], color);
     }
     leds.update();
 }
-
+*/
 
 // function setLight(pos, color){
 //  //console.log("in setLight");
@@ -55,7 +81,27 @@ function setLights(pos, color){
 //   }
 // }
 
+function colorFunction(number){
+  switch(number){
+    case 0:
+      return "[0,0,0]"; //black
+    case 1:
+      return "[0,0,255]"; //blue
+    case 2:
+      return "[255,255,255]"; //white
+    case 3:
+      return "[255,0,0]"; //red
+    case 4:
+      return "[0,255,0]"; //green
+}
 
+var COLORENUM = {
+  White : "[255,255,255]", //white
+  Blue : "[0,0,255]", //blue
+  Green : "[0,255,0]", //green
+  Red : "[255,0,0]", //red
+  Black : "[0,0,0]" //black
+};
 
 function mapColor(color){
   switch(color){
@@ -74,6 +120,40 @@ function mapColor(color){
   }
 }
 
+GRIDSTATES = {
+    "Empty": 0, //black
+    "Water": 1, //blue
+    "Boat": 2, //white
+    "Hit": 3, //red
+    "Miss": 4 //green
+};
+
+function newGridMapper(ledNumber){
+
+  if(ledNumber > 0 && ledNumber < 10){
+    return ["A",ledNumber];
+  }else if(ledNumber > 10 && ledNumber < 19){
+    return ["B",20-ledNumber)];
+  }else if(ledNumber > 20 && ledNumber < 29){
+    return ["C", ledNumber];
+  }else if(lednumber > 30 && lednumber < 39){
+    return ["D", 20-ledNumber];
+  }else if(lednumber > 40 && ledNumber < 49){
+    return ["E", ledNumber];
+  }else if(lednumber > 50 && ledNumber < 59){
+    return ["F", 20-ledNumber];
+  }else if(lednumber > 60 && ledNumber < 69){
+    return ["G", ledNumber];
+  }else if(lednumber > 70 && ledNumber < 79){
+    return ["H", 20-ledNumber];
+  }else if(lednumber > 80 && ledNumber < 89){
+    return ["I", ledNumber];
+  }else if(lednumber > 90 && ledNumber < 99){
+    return ["J", 20-ledNumber];
+  }
+
+}
+
 
 function gridMapper(inputArray){
   //console.log("gridMapper");
@@ -81,40 +161,42 @@ function gridMapper(inputArray){
   var ledArray = [];
 
   for(var i=0; i<inputArray.length;i++){
-  var input = inputArray[i];
-    var _abc = input.substring(0,1);
-    var _123 = parseInt(input.substring(1));
 
-    switch(_abc){
+    var input = inputArray[i];
+
+    var letter = input.substring(0,1);
+    var number = parseInt(input.substring(1));
+
+    switch(letter){
       case "A":
-        ledNumber = 0 + _123-1;
+        ledNumber = 0 + number-1;
         break;
       case "B":
-        ledNumber = 10 + (10 - _123);
+        ledNumber = 10 + (10 - number);
         break;
       case "C":
-        ledNumber = 20 + _123-1;
+        ledNumber = 20 + number-1;
         break;
       case "D":
-        ledNumber = 30 + (10 - _123);
+        ledNumber = 30 + (10 - number);
         break;
       case "E":
-        ledNumber = 40 + _123-1;
+        ledNumber = 40 + number-1;
         break;
       case "F":
-        ledNumber = 50 + (10 - _123);
+        ledNumber = 50 + (10 - number);
         break;
       case "G":
-        ledNumber = 60 + _123-1;
+        ledNumber = 60 + number-1;
         break;
       case "H":
-        ledNumber = 70 + (10 - _123);
+        ledNumber = 70 + (10 - number);
         break;
       case "I":
-        ledNumber = 80 + _123-1;
+        ledNumber = 80 + number-1;
         break;
       case "J":
-        ledNumber = 90 + (10 - _123);
+        ledNumber = 90 + (10 - number);
         break;
       default:
         ledNumber = 0;
